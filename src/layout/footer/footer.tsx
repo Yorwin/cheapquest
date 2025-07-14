9058c0d723c9d65134b69874e3763dc03e687126
Yorwin Rosales <yorwinr295@gmail.com>
2025-07-14 22:36:25 +0200

    commit inicial

---
+++ src/layout/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <p>Hola</p>
+    );
+}
+
+export default Footer;
---
+++ src/layout/header/header.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "@/styles/layout/header.module.scss"
+import Image from "next/image";
+import Link from "next/link";
+import LogoImg from "@/resources/logo/logo.png"
+
+const Header = () => {
+    return (
+        <div className={styles["header-main-container"]}>
+            <ul className={styles["headers-list"]}>
+                <li className={styles["image-item"]}>
+                    <div className={styles["image-container"]}>
+                        <Link href="/" >
+                            <Image
+                                src={LogoImg}
+                                alt="Logo"
+                                fill
+                                style={{ objectFit: 'cover' }}
+                            />
+                        </Link>
+                    </div>
+
+                </li>
+                <li>
+                    <button className={styles["button"]}>
+                        Ofertas
+                        <i className="bi bi-caret-down"></i>
+                    </button>
+                </li>
+                <li className={styles["next-releases-item"]}>
+                    <Link href="/next-releases">Proximos Lanzamientos</Link>
+                </li>
+                <li>
+                    <Link href="/login-page">Iniciar sesión</Link>
+                </li>
+                <li>
+                    <Link href={"/search"}>
+                        <i className="bi bi-search"></i>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
