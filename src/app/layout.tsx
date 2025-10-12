57c840a3f412fee9e3604678c83bd56f14e97550
Yorwin Rosales <yorwinr295@gmail.com>
2025-10-12 14:22:10 +0200

    Google analytics basico implementado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "next/script";
 
 /* Global Styles */
 import "@/app/globals.scss";
@@ -56,6 +57,20 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="es">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body className={`${gontSerrat.variable} ${timeLess.variable}`}>
         <header className="header">
           <Header />
