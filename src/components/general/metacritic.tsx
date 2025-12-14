71ee3085ed101b2ea880330735378953f5f575b9
Yorwin Rosales <52543565+Yorwin@users.noreply.github.com>
2025-12-14 17:34:32 +0100

    Add rawg-rating-data

--- src/components/general/metacritic.tsx
+++ src/components/general/metacritic.tsx
@@ -2,6 +2,17 @@ import styles from "@/styles/components/reviews.module.scss"
 import { getBackupMetacritic } from '@/lib/firebase-cache'
 import Link from "next/link";
 
+interface rawgRating {
+    id: number,
+    title: string,
+    count: number,
+    percent: number,
+}
+
+interface rawgRatingIcons {
+    [name: string]: string;
+}
+
 const MetaCritic = async ({ gameData }: { gameData: any }) => {
 
     /* Metacritic */
@@ -29,18 +40,40 @@ const MetaCritic = async ({ gameData }: { gameData: any }) => {
     /* SteamRating */
     const steamRating = gameData?.reviews.steamRating;
 
-    /* RAWG Cambio */
+    /* Responsive columns */
+    const hasMetacritic = !!metacritic;
+    const hasSteam = !!steamRating;
+    const colClass = hasMetacritic && hasSteam ? 'col-md-6 col-sm-12' : 'col-md-12 col-sm-12';
+
+    /* RAWG */
+    const rawgRatingIcons: rawgRatingIcons = {
+        exceptional: "bi bi-emoji-heart-eyes-fill",
+        recommended: "bi bi-emoji-laughing-fill",
+        meh: "bi bi-emoji-expressionless-fill",
+        skip: "bi bi-emoji-angry-fill",
+    }
+
+    const { rating_average, rating_count, ratings_percentage } = gameData?.reviews.rawGRating;
+    let rawgRatingValue;
+
+    if (rating_average > 4) {
+        rawgRatingValue = statusOfCritic[0];
+    } else if (rating_average >= 3) {
+        rawgRatingValue = statusOfCritic[1];
+    } else {
+        rawgRatingValue = statusOfCritic[2];
+    }
 
     return (
         <section className={styles["metacritic-container"]}>
             <h3>Reviews</h3>
 
             <div className="container-fluid p-0">
-                <div className="row">
+                <div className="row g-5">
 
                     {/* Metacritic */}
                     {metacritic && (
-                        <section className={`col-6 ${styles["review-container"]}`}>
+                        <section className={`${colClass} ${styles["review-container"]}`}>
                             <h4 className={styles["type-of-review"]}>Metacritic</h4>
                             <div className={styles["rating"]}>
                                 {backupMetacritic ? (
@@ -60,7 +93,7 @@ const MetaCritic = async ({ gameData }: { gameData: any }) => {
 
                     {/* Steam Rating */}
                     {steamRating && (
-                        <section className={`col-6 ${styles["review-container"]}`}>
+                        <section className={`${colClass} ${styles["review-container"]}`}>
                             <h4 className={styles["type-of-review"]}>steamRating</h4>
 
                             {/* SteamRating Percent */}
@@ -98,8 +131,7 @@ const MetaCritic = async ({ gameData }: { gameData: any }) => {
                                 <div className={styles["rating"]}>
                                     <span className={`${styles["reviews-amount"]}`}>{steamRating.steamRatingCount}</span>
                                     <div className={styles["text-container"]}>
-                                        <span className={styles["text"]}>Cantidad</span>
-                                        <span className={styles["text"]}>de Reviews</span>
+                                        <span className={styles["text"]}>Cantidad de Reviews</span>
                                     </div>
                                 </div>
                             )}
@@ -107,19 +139,48 @@ const MetaCritic = async ({ gameData }: { gameData: any }) => {
                     )}
 
                     {/* RAWG */}
-                    <section className={`col-6 ${styles["review-container"]}`}>
-                        <h4 className={styles["type-of-review"]}>Rating en RAWG</h4>
-                        <div className={styles["rating"]}>
-                            <span className={`${styles["points"]} ${styles[critic]}`}>{metacritic}</span>
-                            <div className={styles["text-container"]}>
-                                <span className={styles["text"]}>Puntuación</span>
-                                <span className={styles["text"]}>en Metacritic</span>
+                    {gameData?.reviews.rawGRating && (
+                        <section className={`col-md-12 col-sm-12 ${styles["review-container"]}`}>
+                            <h4 className={`${styles["type-of-review"]} ${styles["rawg-title"]}`}>Rating en RAWG</h4>
+
+                            {/* Rating */}
+                            <div className={`${styles["rating"]} ${styles["rawg-rating"]}`}>
+                                <span className={`${styles["points"]} ${styles[rawgRatingValue]}`}>{rating_average}</span>
+                                <div className={styles["text-container"]}>
+                                    <span className={styles["text"]}><i className="bi bi-star-fill"></i> Puntuación</span>
+                                </div>
                             </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </section>
+
+                            {/* Cantidad de Reviews */}
+                            <div className={`${styles["rating"]} ${styles["rawg-rating"]}`}>
+                                <span className={`${styles["reviews-amount"]}`}>{rating_count}</span>
+                                <div className={styles["text-container"]}>
+                                    <span className={styles["text"]}><i className="bi bi-bar-chart-fill"></i> Cantidad de Reviews</span>
+                                </div>
+                            </div>
+
+                            {/* Reviews divididas */}
+                            <div className={styles["divided-reviews"]}>
+                                <ul>
+                                    {ratings_percentage.map((e: rawgRating, index: number) => {
+                                        return (
+                                            <li className={styles["list-element"]} key={index}>
+                                                <i className={rawgRatingIcons[`${e.title}`]}></i>
+                                                <p>
+                                                    <span><strong>Rating:</strong> {e.title.toUpperCase()}</span>
+                                                    <span><strong>Cantidad:</strong> {e.count}</span>
+                                                    <span><strong>Porcentaje:</strong> {e.percent}%</span>
+                                                </p>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        </section>
+                    )}
+                </div >
+            </div >
+        </section >
     )
 };
 
